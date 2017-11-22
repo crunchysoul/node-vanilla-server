efb093e00fb68701bb082bfe1d9835c9d954849d
crunchysoul <zzzcca12@gmail.com>
2017-11-22 13:11:07 +1100

    Add a few routes

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const HTTP = require('http');
 
 const server = HTTP.createServer((request, response) => {
-  console.log('Request received');
+  const path = request.url;
+
+  console.log('Request received', path);
+  if (path === '/') {
+    response.end('Home');
+  } else if (path === '/opensesame') {
+    response.end('Ooh you found the secret');
+  } else if (path === '/postcode.json') {
+    response.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    response.end(`{ "name": "Melbourne", "postcode": 3000 }`);
+  } else {
+    response.end('Back at you');
+  }
 });
 
 const port = 3000;
 
 //start server
-server.listen(3000, (error) => {
+server.listen(port, (error) => {
   console.log(`Server has started at http://local:${port}`);
 })
